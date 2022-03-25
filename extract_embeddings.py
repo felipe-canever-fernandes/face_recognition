608342a3953abf046c06fc4015ff911d089f4d09
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2022-03-25 10:46:13 -0300

    Implement loading face recognizer

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -1,1 +1,34 @@
-print("Hello, Face Recognition world!")
+from argparse import ArgumentParser
+
+from cv2 import dnn
+
+argument_parser: ArgumentParser = ArgumentParser()
+
+argument_parser.add_argument(
+	"-p",
+	"--prototxt",
+	required=True,
+	help="path to Caffe prototxt file"
+)
+
+argument_parser.add_argument(
+	"-c",
+	"--caffe-model",
+	required=True,
+	help="path to Caffe pre-trained model"
+)
+
+argument_parser.add_argument(
+	"-e",
+	"--embedding-model",
+	required=True,
+	help="path to OpenCV's deep learning face embedding model"
+)
+
+arguments = argument_parser.parse_args()
+
+print("Loading face detector...")
+detector = dnn.readNetFromCaffe(arguments.prototxt, arguments.caffe_model)
+
+print("Loading embedding model...")
+embedder = dnn.readNetFromTorch(arguments.embedding_model)
