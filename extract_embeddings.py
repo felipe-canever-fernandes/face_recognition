1b3cdf117f2cba0ff6b684308a545699ac4da3e6
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2022-03-30 11:31:30 -0300

    Implement reading input images

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -1,6 +1,10 @@
 from argparse import ArgumentParser
+from os import path
 
-from cv2 import dnn, dnn_Net
+from cv2 import dnn, dnn_Net, imread
+import imutils
+from imutils import paths
+from numpy import ndarray
 
 argument_parser: ArgumentParser = ArgumentParser()
 
@@ -25,6 +29,13 @@ argument_parser.add_argument(
 	help="path to OpenCV's deep learning face embedding model",
 )
 
+argument_parser.add_argument(
+	"-i",
+	"--input",
+	required=True,
+	help="path to input directory of face images",
+)
+
 arguments = argument_parser.parse_args()
 
 print("Loading face detector...")
@@ -35,3 +46,19 @@ detector: dnn_Net = dnn.readNetFromCaffe(
 
 print("Loading embedding model...")
 embedder: dnn_Net = dnn.readNetFromTorch(arguments.embedding_model)
+
+print("Quantifying faces...")
+image_paths: "list[str]" = list(paths.list_images(arguments.input))
+
+MODEL_IMAGE_SIZE: "tuple[int, int]" = (300, 300)
+
+embeddings: "list[ndarray]" = []
+names: "list[str]" = []
+
+for image_index, image_path in enumerate(image_paths):
+	print(f"Processing image {image_index + 1}/{len(image_paths)}...")
+
+	face_name: str = image_path.split(path.sep)[-2]
+
+	image: ndarray = imread(image_path)
+	image = imutils.resize(image, width=600)
