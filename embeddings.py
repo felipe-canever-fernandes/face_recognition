f4770fb805c1d00bf3af67a9874000b1758f06e8
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2022-04-01 10:06:45 -0300

    Refactor embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -3,23 +3,23 @@ from cv2 import dnn, dnn_Net, imread
 import imutils
 from numpy import array, ndarray
 
-MAXIMUM_IMAGE_WIDTH: int = 600
-IMAGE_BLOB_SIZE: "tuple[int, int]" = (300, 300)
+_MAXIMUM_IMAGE_WIDTH: int = 600
+_IMAGE_BLOB_SIZE: "tuple[int, int]" = (300, 300)
 
-detector: dnn_Net = None
+_detector: dnn_Net = None
 
 def initialize(prototxt_path: str, caffe_model_path: str):
-	global detector
-	detector = dnn.readNetFromCaffe(prototxt_path, caffe_model_path)
+	global _detector
+	_detector = dnn.readNetFromCaffe(prototxt_path, caffe_model_path)
 
 def process_image(image_path: str) -> "tuple[ndarray, ndarray]":
 	image: ndarray = imread(image_path)
-	image = imutils.resize(image, width=MAXIMUM_IMAGE_WIDTH)
+	image = imutils.resize(image, width=_MAXIMUM_IMAGE_WIDTH)
 
 	image_blob: ndarray = dnn.blobFromImage(
-		image=cv2.resize(image, IMAGE_BLOB_SIZE),
+		image=cv2.resize(image, _IMAGE_BLOB_SIZE),
 		scalefactor=1.0,
-		size=IMAGE_BLOB_SIZE,
+		size=_IMAGE_BLOB_SIZE,
 		mean=(104.0, 177.0, 123.0),
 		swapRB=False,
 		crop=False,
@@ -28,9 +28,9 @@ def process_image(image_path: str) -> "tuple[ndarray, ndarray]":
 	return image, image_blob
 
 def detect_faces(image_blob: ndarray) -> ndarray:
-	global detector
-	detector.setInput(image_blob)
-	return detector.forward()
+	global _detector
+	_detector.setInput(image_blob)
+	return _detector.forward()
 
 def get_face(
 	image: ndarray,
